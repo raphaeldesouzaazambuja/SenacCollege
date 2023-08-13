255d23bb0f2118c6b693ec163dcdba9cf383a918
raphael azambuja <raphael@pop-os.localdomain>
2023-08-12 21:13:00 -0300

    added more one question

--- 2-SecondSemester/DataStructures/2-arraysActivities/Question8.java
+++ 2-SecondSemester/DataStructures/2-arraysActivities/Question8.java
@@ -2,21 +2,21 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Question8 
+public class Question8
 {
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
         HashMap<Integer, ArrayList<Object>> employee = new HashMap<>();
-        
+
         String employeeName;
         float employeeSalary;
         float employeeNewSalary;
-        
-        for(int i = 0; i < 3; i++)
+
+        for(int i = 0; i < 20; i++)
         {
             ArrayList<Object> employeeDatas = new ArrayList<>();
-            
+
             System.out.print("Type the name of employee \n-> ");
             employeeName = scan.nextLine();
 
@@ -33,10 +33,10 @@ public class Question8
             employee.put(i, employeeDatas);
         }
         scan.close();
-        
-        for (int i = 0; i < employee.size(); i++) 
+
+        for (int i = 0; i < employee.size(); i++)
         {
             System.out.println(employee.get(i));
         }
-    }    
+    }
 }
